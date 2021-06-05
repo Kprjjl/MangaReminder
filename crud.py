3932842bc38a1258e317b9023c2580a5e5254d07
Kprjjl <69335092+Kprjjl@users.noreply.github.com>
2021-06-05 10:33:10 +0800

    Removed old idea comments

--- crud.py
+++ crud.py
@@ -15,6 +15,17 @@ fieldtypes = {
 # need: checker if name already exists
 
 
+class Manga:
+    def __init__(self, name=None, link=None, current_ch=None, recent_ch=None, interval=None,
+                 up_date=None):
+        self.name = name
+        self.link = link
+        self.current_ch = current_ch
+        self.recent_ch = recent_ch
+        self.interval = interval
+        self.up_date = up_date
+
+
 def check_details(details):
     # check if details is None or is a dictionary
     try:
@@ -56,39 +67,6 @@ def new_manga(name, details=None):
     if details is not None:
         update_details(details)
 
-    # if details is not None and type(details) is dict:
-    # try:
-    #     keys = details.keys()
-    # except AttributeError:
-    #     return
-
-    # required_keys = {'link', 'current_ch', 'recent_ch', 'interval', 'upDate'}
-    # if (required_keys <= keys) is False:
-    #     return "Wrong details passed."
-
-    # accepted_keys = {'name', 'link', 'current_ch', 'recent_ch', 'interval', 'upDate'}
-    # if {*keys} <= accepted_keys:
-    #     return "Wrong details passed."
-
-    # not_null_keys = []
-    # for key in details:
-    #     if details[key] is not None:
-    #         not_null_keys.append(key)
-
-    # query = ""
-    # for key in not_null_keys:
-    #     # query += f"UPDATE Manga SET {key} = ? WHERE name = ?;"
-    #     query += f" {key} = ?,"
-    # query = "UPDATE Manga SET" + query[:-1] + " WHERE name = ?;"
-    #
-    # not_null_keys.append(name)
-    # upd_params = tuple(not_null_keys)
-    #
-    # cursor.execute(query, upd_params)
-
-    # upd_params = [(key, details[key], name) for key in not_null_keys]
-    # cursor.executemany("UPDATE Manga SET ? = ? WHERE name = ?", upd_params)
-
 
 # ---------UPDATING----------
 def update_name(name, new_name):
@@ -124,27 +102,9 @@ def update_details(details):
 
     connect.commit()
     connect.close()
-
-
-# def upd_link(link):
-#     pass
-#
-#
-# def upd_current_ch(current_ch):
-#     pass
-#
-#
-# def upd_recent_ch(recent_ch):
-#     pass
-#
-#
-# def upd_interval(interval):
-#     pass
-#
-#
-# def upd_update(up_date):
-#     pass
 # -------------------------------
+
+
 # ---------DELETING-----------
 def del_manga(name):
     connect = sqlite3.connect(db_file)
